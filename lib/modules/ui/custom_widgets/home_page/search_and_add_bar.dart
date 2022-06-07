0ceb377f39b350b4a7a5a1a718ece89a61343379
Matheus <matheusilva-m@outlook.com>
2022-06-06 21:38:10 -0300

    more dark mode fixes

--- lib/modules/ui/custom_widgets/home_page/search_and_add_bar.dart
+++ lib/modules/ui/custom_widgets/home_page/search_and_add_bar.dart
@@ -3,6 +3,7 @@ import 'package:carsregister/modules/domain/mobx_state/preferences_state.dart';
 import 'package:carsregister/modules/ui/utilities/app_colors.dart';
 import 'package:carsregister/modules/ui/utilities/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class SearchAndAddBar extends StatefulWidget {
   const SearchAndAddBar({Key? key}) : super(key: key);
@@ -15,49 +16,49 @@ class _SearchAndAddBarState extends State<SearchAndAddBar> {
   double searchBarHeight = 0;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(width: MediaQuery.of(context).size.width * .025),
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 500),
-            width: MediaQuery.of(context).size.width * .85,
-            height: searchBarHeight,
-            transformAlignment: Alignment.topCenter,
-            decoration: queryState.showSearchAndAdd
-                ? BoxDecoration(
-                    border: Border(
-                        left: BorderSide(
-                            color: preferencesState.secondaryTextColor),
-                        right: BorderSide(
-                            color: preferencesState.secondaryTextColor),
-                        bottom: BorderSide(
-                            color: preferencesState.secondaryTextColor)))
-                : const BoxDecoration(),
-            child: queryState.showSearchAndAdd
-                ? TextFormField(
-                    controller: queryState.searchBarController,
-                    textAlign: TextAlign.center,
-                    textAlignVertical: TextAlignVertical.center,
-                    cursorColor: preferencesState.secondaryTextColor,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Pesquisar",
-                      hintStyle: AppTextStyles()
-                          .mainTextStyle(color: AppColors().greyColor),
-                    ),
-                    onChanged: (value) {
-                      queryState.searchCarList(searchString: value);
-                    },
-                  )
-                : null,
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
+    return Observer(builder: (_) {
+      return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(width: MediaQuery.of(context).size.width * .025),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              width: MediaQuery.of(context).size.width * .85,
+              height: searchBarHeight,
+              transformAlignment: Alignment.topCenter,
+              decoration: queryState.showSearchAndAdd
+                  ? BoxDecoration(
+                      border: Border(
+                          left: BorderSide(
+                              color: preferencesState.secondaryTextColor),
+                          right: BorderSide(
+                              color: preferencesState.secondaryTextColor),
+                          bottom: BorderSide(
+                              color: preferencesState.secondaryTextColor)))
+                  : const BoxDecoration(),
+              child: queryState.showSearchAndAdd
+                  ? TextFormField(
+                      controller: queryState.searchBarController,
+                      textAlign: TextAlign.center,
+                      textAlignVertical: TextAlignVertical.center,
+                      cursorColor: preferencesState.secondaryTextColor,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Pesquisar",
+                        hintStyle: AppTextStyles()
+                            .mainTextStyle(color: AppColors().greyColor),
+                      ),
+                      onChanged: (value) {
+                        queryState.searchCarList(searchString: value);
+                      },
+                    )
+                  : null,
+            ),
+            GestureDetector(
+              onTap: () {
                 queryState.showSearchAndAdd = !queryState.showSearchAndAdd;
                 if (queryState.showSearchAndAdd) {
                   searchBarHeight = 50;
@@ -66,19 +67,19 @@ class _SearchAndAddBarState extends State<SearchAndAddBar> {
                   queryState.searchBarController.text = "";
                   queryState.carListDisplay = queryState.carList;
                 }
-              });
-            },
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * .1,
-              child: Icon(
-                  queryState.showSearchAndAdd
-                      ? Icons.keyboard_arrow_up
-                      : Icons.search,
-                  color: preferencesState.secondaryTextColor),
+              },
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * .1,
+                child: Icon(
+                    queryState.showSearchAndAdd
+                        ? Icons.keyboard_arrow_up
+                        : Icons.search,
+                    color: preferencesState.secondaryTextColor),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
