1938a04cb41e53d550474ff0a6597f004f054a90
Matheus <matheusilva-m@outlook.com>
2022-06-07 22:01:40 -0300

    darkMode turn true automatically accordingly android settings

--- lib/modules/ui/pages/splashscreen_page.dart
+++ lib/modules/ui/pages/splashscreen_page.dart
@@ -7,6 +7,7 @@ import 'package:carsregister/modules/domain/mobx_state/preferences_state.dart';
 import 'package:carsregister/modules/ui/pages/home_page/home_page.dart';
 import 'package:carsregister/modules/ui/utilities/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -28,8 +29,17 @@ class _SplashscreenPageState extends State<SplashscreenPage> {
     }
   }
 
+  //var brightness = SchedulerBinding.instance.window.platformBrightness;
+
   @override
   void initState() {
+    var brightness = SchedulerBinding.instance.window.platformBrightness;
+    bool isDarkMode = brightness == Brightness.dark;
+
+    if (isDarkMode) {
+      preferencesState.toggleLightMode();
+    }
+
     _getStoragePermission();
     CarsDatabase().initDB();
     registerState.resetValues();
@@ -49,14 +59,14 @@ class _SplashscreenPageState extends State<SplashscreenPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: AppColors().voidColor),
+      decoration: BoxDecoration(color: preferencesState.darkMode ? preferencesState.secondaryColor : preferencesState.primaryColor),
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Lottie.asset("lib/assets/lottie/splashcreen_animation.json",
             repeat: false),
         Text(
           "Mobicar, aluguel de carros inteligente.",
           style: GoogleFonts.roboto(
-            color: preferencesState.primaryTextColor,
+            color: preferencesState.darkMode ? preferencesState.secondaryTextColor : preferencesState.primaryTextColor,
             decoration: TextDecoration.none,
             fontSize: 22,
             fontWeight: FontWeight.w400,
